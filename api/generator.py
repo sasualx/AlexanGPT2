a15fb2f9f0e4dd5ca10ec2403b80122916bb8a6b
sasualx <alexandrusasu42@gmail.com>
2021-01-27 20:49:22 +0100

    structure

---
+++ api/generator.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+generator = pipeline('text-generation',model='/run/media/Data/model_parameters', tokenizer='gpt2', config={'max_length':10000})
+
+def generate_response(input):
+    print(input)
+    output = generator(input, do_sample=True, max_length=len(input.split())+60, top_k=50, top_p=0.95, num_returned_sequences=1)[0]['generated_text']
+    return output[:output.rfind('\n')+1]
